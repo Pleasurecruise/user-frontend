c7599e2f39130710c9174259a3753836d38eceb9
ChingCdesu <chingc.ccmail@gmail.com>
2025-02-03 04:29:18 +0800

    feat: add main page transfer button

--- src/app/[locale]/get-start/page.tsx
+++ src/app/[locale]/get-start/page.tsx
@@ -75,6 +75,12 @@ export default function GetStart() {
           >
             {t('getKey')}
           </Link>
+          <Link
+            href="/transfer"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {t('transfer')}
+            </Link>
           <a href="https://github.com/MirrorChyan/docs" target="_blank" className="text-sm/6 font-semibold">
             {t('apiDoc')}<span aria-hidden="true">&nbsp;→</span>
           </a>
