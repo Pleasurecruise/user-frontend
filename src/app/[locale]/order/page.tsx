c71bb6ac668c3597afe9bd74597dc03a446f0a07
ChingCdesu <chingc.ccmail@gmail.com>
2025-02-09 22:23:34 +0800

    fix: fixed定位

--- src/app/[locale]/order/page.tsx
+++ src/app/[locale]/order/page.tsx
@@ -68,7 +68,7 @@ export default function App() {
             className="max-w-md"
           />
           {(error || timeout) && (
-            <div className="absolute right-4 bottom-4">
+            <div className="fixed right-4 bottom-4">
               <Button onClick={() => router.push('/get-key')}>
                 {t("InputOrderId")}
               </Button>
