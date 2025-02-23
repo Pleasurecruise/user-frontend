21372e062a9a906edf86436e2fb742e740cd4002
dongwlin <dongwlin@foxmail.com>
2025-02-23 13:34:41 +0800

    refactor: enhance layout flexibility with wrap and consistent spacing

--- src/app/[locale]/get-start/page.tsx
+++ src/app/[locale]/get-start/page.tsx
@@ -88,7 +88,7 @@ export default async function GetStart() {
               return <PlanCard key={plan.planId} plan={plan} customOrderId={customOrderId} />
             })}
           </div>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
+          <div className="mt-10 flex flex-wrap items-center justify-center gap-6">
             <Link
               href="/get-key"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
