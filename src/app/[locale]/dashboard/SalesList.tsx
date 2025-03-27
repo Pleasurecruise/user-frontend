56227d864e45f97e1cb6125993b9457078f4c124
YoungYa <1829594026@qq.com>
2025-03-28 02:43:18 +0800

    fix: add every day list

--- src/app/[locale]/dashboard/SalesList.tsx
+++ src/app/[locale]/dashboard/SalesList.tsx
@@ -1,13 +1,70 @@
 import { Table, TableBody, TableColumn, TableHeader, TableRow, TableCell } from "@heroui/react";
 import { RevenueType } from "@/app/[locale]/dashboard/page";
-import {useTranslations} from "next-intl";
+import { useTranslations } from "next-intl";
+import { useMemo } from "react";
+import { groupBy } from "lodash";
 
 type PropsType = {
   listData: RevenueType[]
+  date: string
 }
 
-export default function SalesList({listData}: PropsType) {
+type DataType = {
+  amount: number
+  revenue: number
+  date: string
+}
+
+export default function SalesList({ listData, date }: PropsType) {
   const t = useTranslations("Dashboard.dailyRecord");
+
+  const generateDateRange = () => {
+    const currentMonth = Number(date.slice(5));
+    const year = new Date().getFullYear();
+    const month = [31,
+      year % 4 === 0 && year % 100 !== 0 || year % 400 === 0 ? 29 : 28,
+      31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    const dates: number[] = [];
+    const lastDay = month[currentMonth - 1];
+
+    for (let i = 1; i <= lastDay; i++) {
+      dates.push(i);
+    }
+    return dates;
+  };
+
+  // 初始化日期范围
+  const processedData = useMemo(() => {
+    const groupedData = groupBy(
+      listData.map(item => ({ ...item, activated_at: `${date.slice(5)}-${new Date(item.activated_at).getDate()}` })),
+      (item) => item.activated_at
+    );
+    const dateMap = new Map<string, DataType>();
+    const dateRange = generateDateRange();
+
+    dateRange.forEach((day) => {
+      const key = `${date.slice(5)}-${day}`;
+
+      if (groupedData[key] === undefined) {
+        dateMap.set(key, {
+          amount: 0,
+          revenue: 0,
+          date: key,
+        });
+      } else {
+        const revenue = Number(groupedData[key].reduce((acc, cur) => acc + Number(cur.amount) * Number(cur.buy_count), 0).toFixed(2));
+        const amount = Number(groupedData[key].reduce((acc, cur) => acc + Number(cur.buy_count), 0));
+        dateMap.set(key, {
+          amount,
+          revenue,
+          date: key,
+        });
+      }
+    })
+    return Array.from(dateMap.values());
+  }, [listData]);
+
 
   const columns = [
     {
@@ -15,23 +72,15 @@ export default function SalesList({listData}: PropsType) {
       label: t("date")
     },
     {
-      key: "buy_count",
+      key: "amount",
       label: t("amount")
     },
     {
-      key: "amount",
+      key: "revenue",
       label: t("revenue")
     }
   ]
 
-  const renderCell = (item: RevenueType, columnKey: string): React.ReactNode => {
-    const value = item[columnKey as keyof RevenueType];
-    if(columnKey === "date"){
-      const date = new Date(item.activated_at);
-      return <span>{date.getMonth() + 1 + "-" + date.getDate()}</span>;
-    }
-    return <span>{String(value)}</span>;
-  };
   return (
     <>
       <Table
@@ -41,15 +90,15 @@ export default function SalesList({listData}: PropsType) {
       >
         <TableHeader columns={columns}>
           {(column) => (
-              <TableColumn key={column.key}>
-                {column.label}
-              </TableColumn>
-            )}
+            <TableColumn key={column.key}>
+              {column.label}
+            </TableColumn>
+          )}
         </TableHeader>
-        <TableBody items={listData}>
+        <TableBody items={processedData}>
           {(item) => (
-            <TableRow key={String(item.activated_at)}>
-              {(columnKey) => <TableCell>{renderCell(item, String(columnKey))}</TableCell>}
+            <TableRow key={String(item.date)} className="hover:bg-gray-100">
+              {(columnKey) => <TableCell>{item[columnKey as keyof DataType]}</TableCell>}
             </TableRow>
           )}
         </TableBody>
