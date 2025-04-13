1bd2a20e9a8a6d64f9dad2dc5f2a3015d6519c47
Aliothmoon <aliothmoon@outlook.com>
2025-04-13 21:47:11 +0800

    docs: update integrate url

--- src/components/ProjectIntegratedCard.tsx
+++ src/components/ProjectIntegratedCard.tsx
@@ -19,7 +19,7 @@ import {PlusIcon} from "@heroicons/react/24/outline";
 export default function ProjectIntegratedCard() {
 
   const t = useTranslations("Projects");
-  const url = 'https://qm.qq.com/cgi-bin/qm/qr?k=t3aoBGz5v-sDgcMnfs3WADRaeV2pAVLg&jump_from=webapi&authKey=U2cSrxI5+rQ08EtHzuKoQIfcMPYnytyd25mJ6b5sW5M5xgA6S5naSx83LN4DA3Li'
+  const url = 'https://github.com/MirrorChyan/docs'
   return (
       <div
           className="rounded-lg overflow-hidden shadow-md transition-all duration-300 hover:shadow-lg dark:hover:shadow-primary-500/30 transform hover:-translate-y-1 group cursor-pointer bg-gradient-to-br from-white to-gray-50 dark:from-gray-800 dark:to-gray-900 border border-gray-100 dark:border-gray-600 flex flex-col justify-center items-center p-4 h-full"
