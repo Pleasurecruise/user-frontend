dcf2543cea3e5b4d997bb23ed08cea222b69ba6b
MistEO <mistereo@hotmail.com>
2025-04-22 16:54:11 +0800

    chore: update metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -11,8 +11,8 @@ import { Providers } from "./provider";
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
-  title: "MirrorChyan",
-  description: "Next generation of CDN for developers",
+  title: "Mirror酱",
+  description: "Mirror酱是一个第三方应用分发平台，让开源应用的更新更简单。用户付费使用，收益与开发者共享。此外，Mirror酱本身也是开源的。",
 };
 
 export default async function LocaleLayout({
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "MirrorChyan",
-  description: "Next generation of CDN for developers",
+  title: "Mirror酱",
+  description: "Mirror酱是一个第三方应用分发平台，让开源应用的更新更简单。用户付费使用，收益与开发者共享。此外，Mirror酱本身也是开源的。",
 };
 
 export default function RootLayout({
