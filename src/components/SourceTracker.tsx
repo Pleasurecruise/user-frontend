4a7ccf37b68809e0d08c936ce4852e4d1acc651e
Aliothmoon <aliothmoon@outlook.com>
2025-06-04 00:03:00 +0800

    feat: add source tracker

---
+++ src/components/SourceTracker.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+
+import { useEffect } from "react";
+
+interface SourceTrackerProps {
+  source?: string;
+}
+
+export default function SourceTracker({ source }: SourceTrackerProps) {
+  useEffect(() => {
+    window.localStorage.setItem("source", source ?? "");
+  })
+  return null
+}
+export function getSource() {
+  return localStorage.getItem("source") ?? "";
+}
